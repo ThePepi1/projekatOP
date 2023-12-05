b844b303a1e9a996817199c35e802b9bd31ef808
ThePepi1 <petarpopovicpex@protonmail.com>
2023-12-05 12:44:02 +0100

    Update

--- Sala.py
+++ Sala.py
@@ -1,9 +1,9 @@
 class Sala:
-    def __init__(self, hall_id,hall_name, hall_number_row, hall_signe_site):
+    def __init__(self, hall_id,hall_name, hall_number_row, hall_signe_seat):
         self.hall_id = hall_id
         self.hall_name = hall_name
-        self.hall_number_row = hall_number_row
-        self.hall_signe_site = hall_signe_site
+        self.hall_number_row = int(hall_number_row) 
+        self.hall_signe_seat = int(hall_signe_seat)
         
 Halls = {}
 def validate_name(name):
@@ -14,5 +14,23 @@ def validate_name(name):
     return True
 def validate_number_row(row):
     return row.isdigit()
-def validate_sites(hall_sites):
-    return hall_sites.isdigit()
+def validate_sites(hall_seat):
+    return hall_seat.isdigit()
+def check_seat(hall,seat):
+    size = 0
+    for letter in seat:
+        number = ord(letter) - 65
+        size += number
+
+    if size > hall.hall_signe_seat:
+        return False
+    return True
+def create_seat(number_seat):
+    seat_str = ""
+    while number_seat > 0:
+        
+        letter =chr( ((number_seat - 1)% 26) + 65)
+        seat_str = letter + seat_str
+        number_seat = (number_seat - 1) // 26
+
+    return seat_str
